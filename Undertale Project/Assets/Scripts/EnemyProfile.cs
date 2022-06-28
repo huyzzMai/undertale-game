964f90805631acaaa08842f882b996b1fad24918
huyzzMai <giahuy120701@gmail.com>
2022-06-28 12:16:52 +0700

    Add scripts

---
+++ Undertale Project/Assets/Scripts/EnemyProfile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemyProfile : ScriptableObject
+{
+    public int Hp;
+    public int CharmHp;
+
+    public Sprite EnemyVisual;
+
+    public GameObject[] EnemiesAttacks;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
