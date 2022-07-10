3aea9c6135f20bbf1a3023e8bb60fa54c2ff7228
phucvhd <75359238+phucvhd@users.noreply.github.com>
2022-07-10 19:25:43 +0700

    add collision

--- Undertale Project/Assets/Scripts/Player/PlayerCollision.cs
+++ Undertale Project/Assets/Scripts/Player/PlayerCollision.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    public GameObject explosion;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("hit detected");
-        GameObject e = Instantiate(explosion) as GameObject;
-        e.transform.position = transform.position;
         Destroy(other.gameObject);
         this.gameObject.SetActive(false);
+        Time.timeScale = 0;
     }
 
 }
